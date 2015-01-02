2ccc472ff91c23f646622288149e88ec5b8a9df2
Maxim Grishkin <grishkin.max@gmail.com>
2015-01-02 21:49:04 +0300

    more interesting level but still strange

--- js/levels/level01.js
+++ js/levels/level01.js
@@ -1,6 +1,12 @@
 console.log("level01 imported"); // just to be sure
 
+// level size: 20x15
+// var level01 = {
+//   playerStartPosition: [0, 0],
+//   floor: [[0, 2], [1, 2], [2, 3], [3, 4]],
+// };
+
 var level01 = {
-  playerStartPosition: [0, 0],
-  floor: [[0, 2], [1, 2], [2, 3], [3, 4]],
-};
+  playerStartPosition: [0, 13],
+  floor: [[4, 13], [11, 13], [0, 14], [1, 14], [2, 14], [3, 14], [4, 14], [7, 14], [8, 14], [10, 14], [11, 14], [12, 14], [13, 14], [14, 14], [15, 14], [16, 14], [17, 14], [18, 14], [19, 14]]
+}
